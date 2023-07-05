9a2c38777d16a1a0ca15576d7b8b3540dd8291e9
maxkadushkin <maxim.kadushkin@onlyoffice.com>
2023-07-05 19:49:38 +0300

    [themes] initialize config from native code

--- apps/common/mobile/utils/htmlutils.js
+++ apps/common/mobile/utils/htmlutils.js
@@ -9,7 +9,22 @@ const load_stylesheet = reflink => {
 };
 
 if ( !!window.Android && window.Android.editorConfig ) {
-    window.native = {editorConfig: window.Android.editorConfig()}
+    const native_config = window.Android.editorConfig();
+    const editor_config = {
+        forceedit: !!native_config.forceedit,
+        theme: {
+            type: native_config.theme?.type,
+            select: native_config.theme && native_config.theme.select === false ? false : true,
+        }
+    };
+
+    if ( editor_config.theme.type && editor_config.theme.select === false ) {
+    } else {
+        const saved_theme = !localStorage ? null : JSON.parse(localStorage.getItem("mobile-ui-theme"));
+        editor_config.theme.type = saved_theme?.type || 'system';
+    }
+
+    window.native = {editorConfig: editor_config};
 }
 
 if ( localStorage && localStorage.getItem('mobile-mode-direction') === 'rtl' ) {
@@ -19,11 +34,20 @@ if ( localStorage && localStorage.getItem('mobile-mode-direction') === 'rtl' ) {
     load_stylesheet('./css/framework7.css')
 }
 
-let obj = !localStorage ? {id: 'theme-light', type: 'light'} : JSON.parse(localStorage.getItem("mobile-ui-theme"));
-if ( !obj ) {
-    obj = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ?
-        {id: 'theme-dark', type: 'dark'} : {id: 'theme-light', type: 'light'};
-    localStorage && localStorage.setItem("mobile-ui-theme", JSON.stringify(obj));
+const get_system_theme_type = () =>
+    window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+
+let obj;
+if ( !!window.native ) {
+    obj = {type: window.native.editorConfig.theme.type === 'system' ?
+                    get_system_theme_type() : window.native.editorConfig.theme.type};
+} else {
+    obj = !localStorage ? null : JSON.parse(localStorage.getItem("mobile-ui-theme"));
+    if ( !obj ) {
+        obj = get_system_theme_type() == 'dark' ?
+            {id: 'theme-dark', type: 'dark'} : {id: 'theme-light', type: 'light'};
+        localStorage && localStorage.setItem("mobile-ui-theme", JSON.stringify(obj));
+    }
 }
 
 document.body.classList.add(`theme-type-${obj.type}`, `${window.asceditor}-editor`);
