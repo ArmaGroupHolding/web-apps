fca2a681b67f6428dcf40f93605da91ad870fe37
Eren <kurteren07@gmail.com>
2023-08-25 14:38:20 +0300

    ıNIT

---
+++ build/buildArma.bat
@@ -0,0 +1,21 @@
+SET SUPPORT_EMAIL=info@bul.com.tr
+SET APP_COPYRIGHT="Copyright (c) Arma Bilisim Teknolojileri A.S <%= grunt.template.today(yyyy) %>. All rights reserved"
+SET SUPPORT_URL=https://bul.com.tr/
+SET SALES_EMAIL=sales@bul.com.tr
+SET PUBLISHER_URL=https://bul.com.tr/
+SET PUBLISHER_PHONE=+90 000 000 00 00
+SET PUBLISHER_NAME=Arma Bilisim Teknolojileri A.S
+SET PUBLISHER_ADDRESS=Sinpaş Altın Oran K3 No 94 Cankaya - ANKARA TURKEY
+SET API_URL_EDITING_CALLBACK=https://api.onlyoffice.com/editors/callback
+SET COMPANY_NAME=BUL
+SET APP_TITLE_TEXT=BUL
+SET HELP_URL=https://helpcenter.onlyoffice.com
+SET HELP_CENTER_WEB_DE=https://helpcenter.onlyoffice.com/userguides/docs-de.aspx
+SET HELP_CENTER_WEB_SSE=https://helpcenter.onlyoffice.com/userguides/docs-se.aspx
+SET HELP_CENTER_WEB_PE=https://helpcenter.onlyoffice.com/userguides/docs-pe.aspx
+SET DEFAULT_LANG=en
+SET APP_CUSTOMER_NAME=BUL
+SET PRODUCT_VERSION=7.4.1
+SET BUILD_NUMBER=35
+call npm install
+call npx grunt
